d10a0252ac605fd58685024ab20ab270f215a41a
Miksyu <vozmilova05@gmail.com>
2021-02-04 10:29:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-let getRandomIntInclusive = function (min, max) {
+function getRandomIntInclusive(min, max) {
   if (min >= 0 && max >= 0){
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-return console.log('Введите положительные числа, включая 0')
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+  return Error;
 }
 
 function getRandomArbitrary(min, max) {
   if (min >= 0 && max >= 0){
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
   }
-  return console.log('Введите положительные числа, включая 0');
+  let er = ('Введите положительные числа, включая 0');
+  return er;
 }
+getRandomIntInclusive(4, 10);
+getRandomArbitrary(12, 400);
