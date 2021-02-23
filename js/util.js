72f65af70f5fd3ebc4e397280dafbb879f1c4951
Miksyu <vozmilova05@gmail.com>
2021-02-23 16:38:13 +0300

    Create util.js

---
+++ js/util.js
@@ -0,0 +1,23 @@
+const createArr = ([...source], maxLength) => Array.from(
+  { length: Math.min(source.length, 1 + Math.random() * maxLength | 0) },
+  () => source.splice(Math.random() * source.length | 0, 1)[0]);
+
+function getRandomInt(min, max) {
+  if (min >= 0 && max >= 0){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+  let er = ('Введите положительные числа, включая 0');
+  return er;
+}
+
+const getRandomFloat = (min, max, numbersCountAfterDot) => {
+  if (min >= 0 && max >= 0){
+    const random = Math.random()* (max - min) + min;
+    return random.toFixed(numbersCountAfterDot);
+  }
+  let er = ('Введите положительные числа, включая 0');
+  return er;
+}
+export {createArr, getRandomInt, getRandomFloat};
