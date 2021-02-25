522b6163e67d68f77fca93e4c1cca5f9ebc17944
Miksyu <vozmilova05@gmail.com>
2021-02-25 16:34:59 +0300

    Delete main-zero.js

--- js/main-zero.js
+++
@@ -1,8 +0,0 @@
-function getMainZero(min, max, width) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const randomize = Math.floor(Math.random() * (max - min + 1)) + min;
-  const randomNumber = randomize + '';
-  return randomNumber.length >= width ? randomNumber : new Array(width - randomNumber.length + 1).join(0) + randomNumber;
-}
-export {getMainZero};
