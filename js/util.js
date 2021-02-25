251c4a0c369d0c199ccdff0b9959208381f655ac
Miksyu <vozmilova05@gmail.com>
2021-02-25 16:35:12 +0300

    Update util.js

--- js/util.js
+++ js/util.js
@@ -20,4 +20,13 @@ const getRandomFloat = (min, max, numbersCountAfterDot) => {
   let er = ('Введите положительные числа, включая 0');
   return er;
 }
-export {createArr, getRandomInt, getRandomFloat};
+
+function getMainZero(min, max, width) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randomize = Math.floor(Math.random() * (max - min + 1)) + min;
+  const randomNumber = randomize + '';
+  return randomNumber.length >= width ? randomNumber : new Array(width - randomNumber.length + 1).join(0) + randomNumber;
+}
+
+export {createArr, getRandomInt, getRandomFloat, getMainZero};
